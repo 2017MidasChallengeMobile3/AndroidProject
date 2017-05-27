06ad69c209eb7b54cf8f1413ccf9411688600379
rngksah@naver.com <cjswosla1104!>
2017-05-27 13:00:28 +0900

    ㄱㄱㄱㄱ

---
+++ app/src/main/java/com/midas/mobile3/mobile3/LoginActivity.java
@@ -0,0 +1,68 @@
+package com.midas.mobile3.mobile3;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    EditText editID,editPassword;
+    Button btnLogin, btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        // Actionbar 제거
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        setLayout();
+    }
+
+
+    private void setLayout(){
+        editID = (EditText)findViewById(R.id.login_edit_id);
+        editPassword = (EditText)findViewById(R.id.login_edit_password);
+
+        btnLogin = (Button)findViewById(R.id.login_btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : 실제 로그인 해야됨
+
+                Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+            }
+        });
+
+        btnSignup = (Button)findViewById(R.id.login_btn_signup);
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+}
--- app/src/main/java/com/midas/mobile3/mobile3/MainActivity.java
+++ app/src/main/java/com/midas/mobile3/mobile3/MainActivity.java
@@ -2,12 +2,30 @@ package com.midas.mobile3.mobile3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final long FINISH_INTERVAL_TIME = 2000;
+    private long backPressedTime =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        long tempTime = System.currentTimeMillis();
+        long intervalTiem = tempTime - backPressedTime;
+
+        if(intervalTiem >=0 && intervalTiem <= FINISH_INTERVAL_TIME){
+
+            Toast.makeText(this,"1번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show();
+        }else{
+            finish();
+        }
     }
 }
