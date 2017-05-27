8e59d5a49513f9d41ed515e21207c870f9a4c405
rngksah@naver.com <cjswosla1104!>
2017-05-28 07:50:32 +0900

    관리자 끝

--- app/src/main/java/com/midas/mobile3/mobile3/AdminAddActivity.java
+++ app/src/main/java/com/midas/mobile3/mobile3/AdminAddActivity.java
@@ -1,5 +1,6 @@
 package com.midas.mobile3.mobile3;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,15 +11,23 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.midas.mobile3.mobile3.controller.PointThingAdapter;
 
 public class AdminAddActivity extends AppCompatActivity {
 
+    EditText editTitle, editReqStart,editReqEnd,editExcStart,editExcEnd,editPrice, editURL, editContents;
+    Button btnComplete;
+    Context mCon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add);
+        mCon = this;
         setLayout();
     }
 
@@ -27,6 +36,25 @@ public class AdminAddActivity extends AppCompatActivity {
         toolbar.setTitle("봉사활동 추가");
         setSupportActionBar(toolbar);
 
+        editTitle=(EditText)findViewById(R.id.admin_edit_titlename);
+        editPrice=(EditText)findViewById(R.id.admin_edit_price);
+        editReqStart=(EditText)findViewById(R.id.admin_edit_req_start);
+        editReqEnd=(EditText)findViewById(R.id.admin_edit_req_end);
+        editExcStart=(EditText)findViewById(R.id.admin_edit_req_start);
+        editExcEnd=(EditText)findViewById(R.id.admin_edit_req_end);
+
+        editURL=(EditText)findViewById(R.id.admin_edit_url);
+        editContents=(EditText)findViewById(R.id.admin_edit_contents);
+
+        btnComplete =(Button)findViewById(R.id.admin_btn);
+        btnComplete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : 긁어서 DB ㄱㄱㄱㄱㄱㄱㄱㄱ
+
+                Toast.makeText(mCon,"성공적으로 등록되었습니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
