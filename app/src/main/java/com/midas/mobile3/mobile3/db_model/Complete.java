1077f5572426bd042f0b657c64328e3c27a57f73
maniadb <maniadb@naver.com>
2017-05-27 15:06:25 +0900

    rrr

---
+++ app/src/main/java/com/midas/mobile3/mobile3/db_model/Complete.java
@@ -0,0 +1,11 @@
+package com.midas.mobile3.mobile3.db_model;
+
+/**
+ * Created by tjssm on 2017-05-27.
+ */
+
+public class Complete {
+    public int completeCode;
+    public int userCode;
+    public int vsCode;
+}
