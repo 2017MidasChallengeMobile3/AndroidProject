75f79ebc3b5a5ff57bd19f6a8d084f8b7b030e1b
rngksah@naver.com <cjswosla1104!>
2017-05-28 03:09:18 +0900

    기부 돈!

--- app/src/main/java/com/midas/mobile3/mobile3/BusinessActivity.java
+++ app/src/main/java/com/midas/mobile3/mobile3/BusinessActivity.java
@@ -1,6 +1,8 @@
 package com.midas.mobile3.mobile3;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.midas.mobile3.mobile3.db.RequestDBHelper;
@@ -83,15 +87,21 @@ public class BusinessActivity extends AppCompatActivity {
 
     private void requestBusiness(){
         if(!isDone) {
-            //봉사활동 신청이 안되어 있을 경우
-            RequestDBHelper rdbh = new RequestDBHelper(mcon);
-            //rdbh.insert(Common.userCode, data.voluntaryCode);
+            getIntDialog("기부금액", 10000, 0, 10000, new IntEventListener() {
+                @Override
+                public void intEvent(int val) {
+                    // TODO : val 만큼 기부해 줘야됨(DB)
+                    //봉사활동 신청이 안되어 있을 경우
+                    RequestDBHelper rdbh = new RequestDBHelper(mcon);
+                    //rdbh.insert(Common.userCode, data.voluntaryCode);
 
-            Snackbar.make(fab, data.businessName + " 신청이 완료되었습니다.", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            fab.setImageResource(R.drawable.ic_remove_black_24dp);
-            btnRequest.setText("기부 완료");
-            isDone=true;
+                    Snackbar.make(fab, data.businessName + " 신청이 완료되었습니다.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    fab.setImageResource(R.drawable.ic_remove_black_24dp);
+                    btnRequest.setText("기부 완료");
+                    isDone=true;
+                }
+            });
         }else{
             Snackbar.make(fab, "이미 기부하였습니다.", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
@@ -107,4 +117,90 @@ public class BusinessActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    interface IntEventListener{
+        abstract void intEvent(int val);
+    }
+
+
+    private void getIntDialog(String title, int initValue, final int min, final int max, final IntEventListener eventListener) {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_get_integer);
+
+        final LinearLayout rootView = (LinearLayout) dialog.findViewById(R.id.root_view);
+        final SeekBar seekBar = (SeekBar) rootView.findViewById(R.id.seek_bar);
+
+        final int[] curValue = {initValue}; // 현재값
+
+        ((TextView) (rootView.findViewById(R.id.dialog_title))).setText(title);
+        ((TextView) (rootView.findViewById(R.id.dialog_number))).setText("" + curValue[0]);
+
+        rootView.findViewById(R.id.plus_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (curValue[0] < max) {
+                    curValue[0]++;
+                    ((TextView) (rootView.findViewById(R.id.dialog_number))).setText("" + curValue[0]);
+                    seekBar.setProgress(curValue[0] - min);
+                }
+            }
+        });
+
+        rootView.findViewById(R.id.minus_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (curValue[0] > min) {
+                    curValue[0]--;
+                    ((TextView) (rootView.findViewById(R.id.dialog_number))).setText("" + curValue[0]);
+                    seekBar.setProgress(curValue[0] - min);
+                }
+            }
+        });
+
+        seekBar.setMax(max - min);
+        seekBar.setProgress(curValue[0] - min);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                curValue[0] = i + min;
+                ((TextView) (rootView.findViewById(R.id.dialog_number))).setText("" + curValue[0]);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        rootView.findViewById(R.id.dialog_ok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                eventListener.intEvent(curValue[0]);
+                dialog.dismiss();
+            }
+        });
+
+        rootView.findViewById(R.id.dialog_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+
+            }
+        });
+
+        dialog.show();
+    }
+
 }
